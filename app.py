8260b985f00a3e34e7965c5389e971d8e965719f
anasrazakhan7866-arch <anasrazakhan7866@gmail.com>
2026-02-25 14:49:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ app.secret_key = "pharma_secret"
 
 # ---------- DATABASE CONNECTION ----------
 def get_db():
-   conn = mysql.connector.connect(
-    host=os.environ.get("MYSQLHOST"),
+    return  mysql.connector.connect(
+        host=os.environ.get("MYSQLHOST"),
     user=os.environ.get("MYSQLUSER"),
     password=os.environ.get("MYSQLPASSWORD"),
     database=os.environ.get("MYSQLDATABASE"),
-    port=os.environ.get("MYSQLPORT")
-   )
+    port=int(os.environ.get("MYSQLPORT"))
+    )
+
 
 
 # ---------- HOME ----------
@@ -319,5 +320,6 @@ import os
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
